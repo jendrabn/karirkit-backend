3bf288b722813be8c11173ec53ae07c602bc5fe2
jendrabn <jendra455@gmail.com>
2026-09-01 08:07:13 +0700

    refactor: update project structure and configurations
    - Changed the entry script from 'dist/server.js' to 'src/server.js' in ecosystem.config.js.
    - Updated package.json to reflect the new project name 'karirkit-backend' and version '1.0.0'.
    - Modified scripts to use 'bun' commands and adjusted dependencies.
    - Revised tsconfig.json to use ESNext module and target, and updated compiler options for better compatibility with Bun.

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,7 +1,7 @@
 module.exports = {
     apps: [{
         name: 'karirkit-backend',
-        script: 'dist/server.js',
+        script: 'src/server.js',
         interpreter: process.env.BUN_BIN || `${process.env.HOME}/.bun/bin/bun`,
         cwd: '/var/www/karirkit-backend',
         env: { NODE_ENV: 'production', PORT: 3000 }
